1bc3814020aabbb4c5b083a5865f9c480adec339
rajkumar natarajan <raj.cs.psg@gmail.com>
2018-09-17 01:28:50 -0500

    updated kafka schema ref=gistry UI

--- Kafka-schema-registry/deployMisc.sh
+++ Kafka-schema-registry/deployMisc.sh
@@ -16,3 +16,22 @@ echo "192.168.10.35 kafka1
 
 sudo mkdir -p Installed
 cd Installed
+
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+    
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+   
+sudo apt-get install -y docker-ce docker-compose
+
+
+sudo usermod -aG docker $(whoami)
