734ef0d2f14f61244e803213bd96dc6f64711dc7
rajkumar natarajan <raj.cs.psg@gmail.com>
2018-11-15 21:07:27 -0500

    Updated Just NameNode DataNode

--- JustNameNodeDataNode/datanode.sh
+++ JustNameNodeDataNode/datanode.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-sudo yum install -y vim nano tar nmap ca-certificates net-tools zip unzip wget
+#sudo su - vagrant
+sudo yum install -y vim nano tar nmap ca-certificates net-tools zip unzip wget bind-utils
 sudo echo "%admin ALL=(ALL) ALL" >>sudo tee /etc/sudoers
-sudo ssh-keygen -t dsa -P '' -f ~/.ssh/id_dsa
-sudo cat ~/.ssh/id_dsa.pub >> ~/.ssh/authorized_keys
+ssh-keygen -t dsa -P '' -f ~/.ssh/id_dsa
+cat ~/.ssh/id_dsa.pub >> ~/.ssh/authorized_keys
 
 echo "192.168.10.34 datanode1.com
 192.168.10.35 datanode2.com
+192.168.10.36 datanode3.com
 192.168.10.33 snamenode.com
 192.168.10.32 namenode.com" | sudo tee --append /etc/hosts
 
+hostnamectl set-hostname "datanode$1.com"
 wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 rpm -ivh epel-release-6-8.noarch.rpm
 yum --enablerepo=epel -y install sshpass
@@ -18,19 +21,23 @@ sudo rpm -ivh jdk-8u181-linux-x64.rpm
 sudo cp /vagrant/hadoop-2.9.1.tar.gz .
 sudo tar -xvzf hadoop-2.9.1.tar.gz
 
-echo "vagrant ALL=NOPASSWD: /home/vagrant/hadoop-2.9.1/sbin/start-dfs.sh" >> sudo tee /etc/sudoers
-echo pwd
-pwd
+echo "vagrant ALL=NOPASSWD: /home/vagrant/hadoop-2.9.1/sbin/start-dfs.sh" | sudo tee --append /etc/sudoers
+
 cd hadoop-2.9.1
 sudo rm -rf /home/vagrant/hadoop-2.9.1/etc/hadoop/slaves
 sudo touch /home/vagrant/hadoop-2.9.1/etc/hadoop/slaves
 echo "datanode1.com
-datanode2.com" | sudo tee --append /home/vagrant/hadoop-2.9.1/etc/hadoop/slaves
+datanode2.com
+datanode3.com" | sudo tee --append /home/vagrant/hadoop-2.9.1/etc/hadoop/slaves
 
 sudo rm -rf /home/vagrant/hadoop-2.9.1/etc/hadoop/hadoop-env.sh
 sudo rm -rf /home/vagrant/hadoop-2.9.1/etc/hadoop/core-site.xml
 sudo rm -rf /home/vagrant/hadoop-2.9.1/etc/hadoop/hdfs-site.xml
+sudo rm -rf /home/vagrant/hadoop-2.9.1/etc/hadoop/mapred-site.xml
+sudo rm -rf /home/vagrant/hadoop-2.9.1/etc/hadoop/yarn-site.xml
 
 sudo cp /vagrant/hadoop-env.sh /home/vagrant/hadoop-2.9.1/etc/hadoop/
 sudo cp /vagrant/core-site.xml /home/vagrant/hadoop-2.9.1/etc/hadoop/
 sudo cp /vagrant/hdfs-site-nn.xml /home/vagrant/hadoop-2.9.1/etc/hadoop/hdfs-site.xml
+sudo cp /vagrant/mapred-site.xml /home/vagrant/hadoop-2.9.1/etc/hadoop/
+sudo cp /vagrant/yarn-site.xml /home/vagrant/hadoop-2.9.1/etc/hadoop/
--- JustNameNodeDataNode/namenode.sh
+++ JustNameNodeDataNode/namenode.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-sudo yum install -y vim nano tar nmap ca-certificates net-tools zip unzip wget
+#sudo su - vagrant
+sudo yum install -y vim nano tar nmap ca-certificates net-tools zip unzip wget bind-utils
 sudo echo "%admin ALL=(ALL) ALL" >>sudo tee /etc/sudoers
-sudo ssh-keygen -t dsa -P '' -f ~/.ssh/id_dsa
-sudo cat ~/.ssh/id_dsa.pub >> ~/.ssh/authorized_keys
+ssh-keygen -t dsa -P '' -f ~/.ssh/id_dsa
+cat ~/.ssh/id_dsa.pub >> ~/.ssh/authorized_keys
 
 echo "192.168.10.34 datanode1.com
 192.168.10.35 datanode2.com
+192.168.10.36 datanode3.com
 192.168.10.33 snamenode.com
 192.168.10.32 namenode.com" | sudo tee --append /etc/hosts
 
@@ -14,6 +16,7 @@ sudo rpm -ivh jdk-8u181-linux-x64.rpm
 sudo cp /vagrant/hadoop-2.9.1.tar.gz .
 sudo tar -xvzf hadoop-2.9.1.tar.gz
 
+hostnamectl set-hostname "namenode.com"
 wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 rpm -ivh epel-release-6-8.noarch.rpm
 yum --enablerepo=epel -y install sshpass
@@ -24,12 +27,17 @@ cd hadoop-2.9.1
 sudo rm -rf etc/hadoop/slaves
 sudo touch etc/hadoop/slaves
 echo "datanode1.com
-datanode2.com" | sudo tee --append /home/vagrant/hadoop-2.9.1/etc/hadoop/slaves
+datanode2.com
+datanode3.com" | sudo tee --append /home/vagrant/hadoop-2.9.1/etc/hadoop/slaves
 
 sudo rm -rf /home/vagrant/hadoop-2.9.1/etc/hadoop/hadoop-env.sh
 sudo rm -rf /home/vagrant/hadoop-2.9.1/etc/hadoop/core-site.xml
 sudo rm -rf /home/vagrant/hadoop-2.9.1/etc/hadoop/hdfs-site.xml
+sudo rm -rf /home/vagrant/hadoop-2.9.1/etc/hadoop/mapred-site.xml
+sudo rm -rf /home/vagrant/hadoop-2.9.1/etc/hadoop/yarn-site.xml
 
 sudo cp /vagrant/hadoop-env.sh /home/vagrant/hadoop-2.9.1/etc/hadoop/
 sudo cp /vagrant/core-site.xml /home/vagrant/hadoop-2.9.1/etc/hadoop/
 sudo cp /vagrant/hdfs-site-nn.xml /home/vagrant/hadoop-2.9.1/etc/hadoop/hdfs-site.xml
+sudo cp /vagrant/mapred-site.xml /home/vagrant/hadoop-2.9.1/etc/hadoop/
+sudo cp /vagrant/yarn-site.xml /home/vagrant/hadoop-2.9.1/etc/hadoop/
